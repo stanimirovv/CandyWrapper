f717aaade2e61bfc48a03b51d91f0021621db8fb
Zlatin Stanimirov <zlatin_stanimirovv@abv.bg>
2013-09-30 20:59:57 +0300

    asd2

--- BETAJS5.js
+++ BETAJS5.js
@@ -55,9 +55,9 @@ function updateStr() {
     }
     console.log(middle);
 	//add string which will update the visuals of the code here.
-    var updateGui = ";document.getElementById('candies_eaten').innerHTML = 'You have eaten  ' + candies.nbrEaten + 'candies!'"+
-                    +"document.getElementById('candies_thrown').innerHTML = 'You threw    ' + candies.nbrThrown + 'candies on the ground :('" +
-                    "document.getElementById('lollipops').innerHTML = 'You have    ' + lollipops.nbrOwned + 'lollipops'";
+    var updateGui = ";document.getElementById('candies_eaten').innerHTML = 'You have eaten  ' + candies.nbrEaten + 'candies!';"+
+                    +"document.getElementById('candies_thrown').innerHTML = 'You threw    ' + candies.nbrThrown + 'candies on the ground :(';" +
+                    "document.getElementById('lollipops').innerHTML = 'You have    ' + lollipops.nbrOwned + 'lollipops';";
     middle += updateGui;
     console.log(updateGui);
     console.log(middle);
